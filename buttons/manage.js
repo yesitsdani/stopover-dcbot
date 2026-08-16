b3fe1a04d26c5fc284261a4fae158cba87a41f1a
yesitsdani <aidanwilleon@gmail.com>
2026-08-16 21:50:10 +0800

    fixed manage command

--- buttons/manage.js
+++ buttons/manage.js
@@ -1,4 +1,4 @@
-const { ActionRowBuilder, UserSelectMenuBuilder, StringSelectMenuBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const { ActionRowBuilder, UserSelectMenuBuilder, StringSelectMenuBuilder, ButtonBuilder, ButtonStyle, StringSelectMenuOptionBuilder } = require("discord.js");
 const { getMemberName, createEmbedStandard } = require("../modules");
 
 module.exports = {
@@ -25,26 +25,34 @@ module.exports = {
         if (action == "batch") {
             const actions = new ActionRowBuilder()
                 .addComponents(
-                    new ButtonBuilder()
+                    /* new ButtonBuilder()
                     .setCustomId(`manage.${uid}.${targetUid}.batchSet.one`)
-                    .setLabel(`1NDERFUL`)
-                    .setStyle(ButtonStyle.Secondary),
-                    new ButtonBuilder()
-                    .setCustomId(`manage.${uid}.${targetUid}.batchSet.two`)
-                    .setLabel(`2ND2NONE`)
-                    .setStyle(ButtonStyle.Secondary),
-                    new ButtonBuilder()
-                    .setCustomId(`manage.${uid}.${targetUid}.batchSet.three`)
-                    .setLabel(`3UPHORIA`)
-                    .setStyle(ButtonStyle.Secondary),
-                    new ButtonBuilder()
-                    .setCustomId(`manage.${uid}.${targetUid}.batchSet.four`)
-                    .setLabel(`4TUNE`)
-                    .setStyle(ButtonStyle.Secondary),
-                    new ButtonBuilder()
-                    .setCustomId(`manage.${uid}.${targetUid}.batchSet.five`)
-                    .setLabel(`5TAR`)
-                    .setStyle(ButtonStyle.Secondary)
+                    .setLabel(`1NDERFUL`) 
+                    .setStyle(ButtonStyle.Secondary)*/
+                    new StringSelectMenuBuilder()
+                        .setCustomId(`manage.${uid}.${targetUid}.batchSet`)
+                        .setPlaceholder(`Select Batch Role to Set`)
+                        .setMaxValues(1)
+                        .addOptions(
+                            new StringSelectMenuOptionBuilder()
+                                .setValue(`1513016636280541234`)
+                                .setLabel(`Batch 1NDERFUL`),
+                            new StringSelectMenuOptionBuilder()
+                                .setValue(`1513016746326495286`)
+                                .setLabel(`Batch 2ND2NONE`),
+                            new StringSelectMenuOptionBuilder()
+                                .setValue(`1515313966845267988`)
+                                .setLabel(`Batch 3UPHORIA`),
+                            new StringSelectMenuOptionBuilder()
+                                .setValue(`1521884104738607104`)
+                                .setLabel(`Batch 4TUNE`),
+                            new StringSelectMenuOptionBuilder()
+                                .setValue(`1523321069438636203`)
+                                .setLabel(`Batch 5TAR`),
+                            new StringSelectMenuOptionBuilder()
+                                .setValue(`1538537845302632548`)
+                                .setLabel(`Batch 6ORGEOUS`)
+                        )
                 )
 
             return await interaction.update({ components: [actions] });
@@ -73,42 +81,11 @@ module.exports = {
                 await member.roles.add("1504338673900982272");
                 content += ` NSFW access has been granted by ${getMemberName(origMember)} (<@${uid}>)`;
             }
-        } else if (action == "batchSet") {
-            let batchRoles = ['1513016636280541234', '1513016746326495286', '1515313966845267988', '1521884104738607104', '1523321069438636203'];
-            let batchRoleToAdd = '';
-            content += ` batch role has been set to`;
-            const batchNumArg = args.shift();
-
-            if (batchNumArg == "one") {
-                content += ` \`1NDERFUL\``;
-                batchRoles = batchRoles.filter(itm => itm != '1513016636280541234');
-                batchRoleToAdd = "1513016636280541234";
-            } else if (batchNumArg == "two") {
-                content += ` \`2ND2NONE\``;
-                batchRoles = batchRoles.filter(itm => itm != '1513016746326495286');
-                batchRoleToAdd = "1513016746326495286";
-            } else if (batchNumArg == "three") {
-                content += ` \`3UPHORIA\``;
-                batchRoles = batchRoles.filter(itm => itm != '1515313966845267988');
-                batchRoleToAdd = "1515313966845267988";
-            } else if (batchNumArg == "four") {
-                content += ` \`4TUNE\``;
-                batchRoles = batchRoles.filter(itm => itm != '1521884104738607104');
-                batchRoleToAdd = "1521884104738607104";
-            } else if (batchNumArg == "five") {
-                content += ` \`5TAR\``;
-                batchRoles = batchRoles.filter(itm => itm != '1523321069438636203');
-                batchRoleToAdd = "1523321069438636203";
-            }
-
-            await member.roles.remove(batchRoles);
-            await member.roles.add(batchRoleToAdd);
-            content += `  by ${getMemberName(origMember)} (<@${uid}>)`;
         }
 
         const embed = createEmbedStandard()
-        .setThumbnail(member.user.avatarURL())
-        .setDescription(content)
+            .setThumbnail(member.user.avatarURL())
+            .setDescription(content)
 
         const modLogs = await interaction.guild.channels.fetch('1518574560033636412');
         await modLogs.send(content);
--- menus/manage.js
+++ menus/manage.js
@@ -1,5 +1,5 @@
 const { MessageFlags, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
-const { createEmbedStandard } = require(`../modules`);
+const { createEmbedStandard, getMemberName } = require(`../modules`);
 
 module.exports = {
     name: "manage",
@@ -7,68 +7,98 @@ module.exports = {
         const uid = args.shift();
         if (interaction.user.id != uid) return interaction.reply({ content: `This is not for you`, flags: MessageFlags.Ephemeral });
 
-        const user = interaction.users.first();
-        const member = interaction.members.first();
-        const targetUid = interaction.values[0];
+        if (!args[1]) {
+            const user = interaction.users.first();
+            const member = interaction.members.first();
+            const targetUid = interaction.values[0];
 
-        if (user.bot) return interaction.reply({ content: `This user is a bot`, flags: MessageFlags.Ephemeral });
-        await interaction.deferUpdate();
+            if (user.bot) return interaction.reply({ content: `This user is a bot`, flags: MessageFlags.Ephemeral });
+            await interaction.deferUpdate();
 
-        let content = `# <@${targetUid}>\n> Passerby Information\n\n`;
+            let content = `# <@${targetUid}>\n> Passerby Information\n\n`;
 
-        if (member.roles.cache.has("1513016636280541234")) {
-            content += `<@&1513016636280541234>`;
-        } else if (member.roles.cache.has("1513016746326495286")) {
-            content += `<@&1513016746326495286>`;
-        } else if (member.roles.cache.has("1515313966845267988")) {
-            content += `<@&1515313966845267988>`;
-        } else if (member.roles.cache.has("1521884104738607104")) {
-            content += `<@&1521884104738607104>`;
-        } else if (member.roles.cache.has("1523321069438636203")) {
-            content += `<@&1523321069438636203>`;
-        } else {
-            content += `\`NO BATCH ROLE!\``;
+            if (member.roles.cache.has("1513016636280541234")) {
+                content += `<@&1513016636280541234>`;
+            } else if (member.roles.cache.has("1513016746326495286")) {
+                content += `<@&1513016746326495286>`;
+            } else if (member.roles.cache.has("1515313966845267988")) {
+                content += `<@&1515313966845267988>`;
+            } else if (member.roles.cache.has("1521884104738607104")) {
+                content += `<@&1521884104738607104>`;
+            } else if (member.roles.cache.has("1523321069438636203")) {
+                content += `<@&1523321069438636203>`;
+            } else {
+                content += `\`NO BATCH ROLE!\``;
+            }
+
+            content += `\nHas Selfie Access: `;
+            if (member.roles.cache.has("1518254127006744696")) {
+                content += `\`YES\``;
+            } else {
+                content += `\`NO\``;
+            }
+
+            content += `\nHas NSFW Access: `;
+            if (member.roles.cache.has("1504338673900982272")) {
+                content += `\`YES\``;
+            } else {
+                content += `\`NO\``;
+            }
+
+            const embed = createEmbedStandard()
+                .setThumbnail(user.avatarURL())
+                .setDescription(content);
+
+            const actions = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setCustomId(`manage.${uid}.${targetUid}.selfie`)
+                        .setLabel(`Toggle Selfie Access`)
+                        .setStyle(ButtonStyle.Primary),
+                    new ButtonBuilder()
+                        .setCustomId(`manage.${uid}.${targetUid}.nsfw`)
+                        .setLabel(`Toggle NSFW Access`)
+                        .setStyle(ButtonStyle.Danger),
+                    new ButtonBuilder()
+                        .setCustomId(`manage.${uid}.${targetUid}.batch`)
+                        .setLabel(`Change Batch`)
+                        .setStyle(ButtonStyle.Secondary),
+                    new ButtonBuilder()
+                        .setCustomId(`manage.${uid}.${targetUid}.other`)
+                        .setLabel(`Other Passerby`)
+                        .setStyle(ButtonStyle.Success)
+                )
+
+            await interaction.editReply({ embeds: [embed], components: [actions] });
+        } else if (args[1] == "batchSet") {
+            await interaction.deferUpdate();
+            const targetUid = args.shift();
+
+            const member = await interaction.guild.members.fetch(targetUid);
+            const origMember = await interaction.guild.members.fetch(uid);
+
+            let batchRoles = ['1513016636280541234', '1513016746326495286', '1515313966845267988', '1521884104738607104', '1523321069438636203', '1538537845302632548'];
+            let batchRoleToAdd = interaction.values[0];
+            let content = `<@${targetUid}>'s batch role has been set to <@&${batchRoleToAdd}>`;
+            const batchNumArg = args.shift();
+            batchRoles = batchRoles.filter(itm => itm != batchRoleToAdd);
+
+            await member.roles.remove(batchRoles);
+
+            await member.roles.add(batchRoleToAdd);
+            content += `  by ${getMemberName(origMember)} (<@${uid}>)`;
+
+            const embed = createEmbedStandard()
+                .setDescription(content)
+
+            const modLogs = await interaction.guild.channels.fetch('1518574560033636412');
+            await modLogs.send({ embeds: [embed] });
+
+            embed
+                .setThumbnail(member.user.avatarURL())
+
+            return await interaction.editReply({ embeds: [embed], components: [] });
         }
 
-        content += `\nHas Selfie Access: `;
-        if (member.roles.cache.has("1518254127006744696")) {
-            content += `\`YES\``;
-        } else {
-            content += `\`NO\``;
-        }
-
-        content += `\nHas NSFW Access: `;
-        if (member.roles.cache.has("1504338673900982272")) {
-            content += `\`YES\``;
-        } else {
-            content += `\`NO\``;
-        }
-
-        const embed = createEmbedStandard()
-        .setThumbnail(user.avatarURL())
-        .setDescription(content);
-
-        const actions = new ActionRowBuilder()
-        .addComponents(
-            new ButtonBuilder()
-            .setCustomId(`manage.${uid}.${targetUid}.selfie`)
-            .setLabel(`Toggle Selfie Access`)
-            .setStyle(ButtonStyle.Primary),
-            new ButtonBuilder()
-            .setCustomId(`manage.${uid}.${targetUid}.nsfw`)
-            .setLabel(`Toggle NSFW Access`)
-            .setStyle(ButtonStyle.Danger),
-            new ButtonBuilder()
-            .setCustomId(`manage.${uid}.${targetUid}.batch`)
-            .setLabel(`Change Batch`)
-            .setStyle(ButtonStyle.Secondary),
-            new ButtonBuilder()
-            .setCustomId(`manage.${uid}.${targetUid}.other`)
-            .setLabel(`Other Passerby`)
-            .setStyle(ButtonStyle.Success)
-        )
-
-        await interaction.editReply({ embeds: [embed], components: [actions] });
-        
     }
 }
